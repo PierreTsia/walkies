ffb278ece22fdca9a027b60c7df0ef649485ca55
Pierre_T <pierre.tsiakkaros@gmail.com>
2024-11-06 18:58:08 +0100

    add axios instance still works

--- src/hooks/useGetMessage.ts
+++ src/hooks/useGetMessage.ts
@@ -5,14 +5,16 @@ import {
   UseSuspenseQueryResult,
 } from '@tanstack/react-query'
 import axios from 'axios'
+import { axiosInstance } from '../../axios-instance'
 
 // Creating a reusable hook to get messages, need to use axios here since Next patches fetch
 // and causes issues with msw
+
 const useGetMessage = (): UseSuspenseQueryResult<{ message: string }, Error> =>
   useSuspenseQuery({
     queryKey: ['/api/message'],
     queryFn: async () => {
-      const { data } = await axios.get('/api/message')
+      const { data } = await axiosInstance.get('/api/message')
 
       return data
     },
